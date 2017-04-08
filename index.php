3998c198710336ad820d339dc04de0ea15701927
Elias Ojala <ojala.s.elias@gmail.com>
2017-04-08 14:07:50 +0300

    Remove old license text & code

--- index.php
+++ index.php
@@ -1,29 +1,19 @@
 <?php
-# die(':(');
-$title = "Etusivu";
-$keywords = "";
-$description = "";
-$lisakoodi = '<!-- index.php -->';
+	$title = "Etusivu";
+	$keywords = "";
+	$description = "";
+	$lisakoodi = '<!-- index.php -->';
 
-include_once "includes/top.php";
-
+	include_once "includes/top.php";
 ?>
 
 <h1>Hei, ja tervetuloa sivupohjaani!</h1>
 
 <p>
-						Tervetuloa <b>2k15</b>-sivupohjaani! Saat käyttää sivupohjaa lähes miten vain haluat, kunhan noudatat seuraavia ehtoja:<br>
-
-						<ul>
-							<li>Saat poistaa "Layout made by <a href="https://theel0ja.info">TheEl0ja</a>"-tekstin.</li>
-							<li><b>Et</b> saa jakaa/myydä sivupohjaa muille, paitsi antamalla linkin <a href="https://theel0ja.info/sivupohjat">tänne!</a></li>
-						</ul>
+	Tervetuloa <b>2k15</b>-sivupohjaani! Saat käyttää sivupohjaa lähes miten vain haluat, kunhan noudatat 
+	<a href="https://github.com/theel0ja/2k15-html/blob/master/LICENSE.md">lisenssin</a> ehtoja.
 </p>
 
-<p>Tarvitsetko muuten webhotellin sivuillesi? Kurkkaappas suomalaista <a href="https://net9.fi/aff.php?aff=174">Net9</a>:n sivuille, sieltä saa varmasti hyvän asiakaspalvelun ja edulliset tuotteet.</p>
-
 <?php
-
-include_once "includes/bottom.php";
-
+	include_once "includes/bottom.php";
 ?>
